021afa0ae886c0d4affa022cf09975707e6b0b16
Frank <benusfrank@gmail.com>
2016-10-18 22:46:24 +0200

    Added url

--- js/app.js
+++ js/app.js
@@ -2,16 +2,40 @@
 
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
+
+};
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+  $('#lbl_currenturl').text(window.location.href);
+});
+
+$('#btn_authorize').click(function() {
 	log('start init');
 	var authClient = new OktaAuth({
 	  url: 'https://randomcompany.okta-emea.com',
 	  clientId: 'ZjHH7CYE8VKqjhoC7dAI'
 	});
-		log('end init');
-};
+	log('end init');
+	log('end init');
 
-$('#btn_authorize').click(function() {
-	log('Click');
+
+	var options = {
+	  responseType: 'id_token',
+	  scopes: ['openid', 'email']
+	};
+	authClient.token.getWithRedirect(options);
+	
+	// If the token is in the url after the redirect
+	authClient.token.parseFromUrl()
+	.then(function(tokenOrTokens) {
+	  // Manage token or tokens
+	})
+	.catch(function(err) {
+	  // Handle OAuthError
+	});
+
+
 });
 
 $('#btn_openbrowser').click(function() {
