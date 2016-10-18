e2030c8c78ebbfd5568a295b815c7c6e29829845
Frank <benusfrank@gmail.com>
2016-10-18 21:24:27 +0200

    test for inapt plugin

--- js/app.js
+++ js/app.js
@@ -11,4 +11,5 @@ $('#btn_authorize').click(function() {
 
 $('#btn_openbrowser').click(function() {
 	log('Open browser');
+	var ref = cordova.InAppBrowser.open('http://www.aap.nl', '_blank', 'location=no');
 });
